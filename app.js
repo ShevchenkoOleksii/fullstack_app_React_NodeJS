a3324aee0b27a455522011a5ea5f5c19e22271aa
ShevchenkoOleksii <84289820+ShevchenkoOleksii@users.noreply.github.com>
2022-03-13 19:24:44 +0200

    'test_commit'

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, "/client/build")));
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
 });
-// const PORT = config.get("port") || 5000
+// const PORT = config.get("port") || 50002
 const PORT = process.env.PORT || 5000
 
 async function start() {
